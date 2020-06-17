a52bec558051a16dfdafa18e1512e4f7e76dbf42
uday.singh <uday.singh@host.docker.internal>
2020-06-18 01:55:59 +0530

    Changes done in the POM file and in Main class

--- src/main/java/com/websystique/springboot/SpringBootStandAloneApp.java
+++ src/main/java/com/websystique/springboot/SpringBootStandAloneApp.java
@@ -2,15 +2,23 @@ package com.websystique.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @EnableAutoConfiguration
 @ComponentScan("com.websystique.springboot")
 //@SpringBootApplication(scanBasePackages={"com.websystique.springboot"})// same as @Configuration @EnableAutoConfiguration @ComponentScan
-public class SpringBootStandAloneApp {
+public class SpringBootStandAloneApp extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStandAloneApp.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootStandAloneApp.class);
+	
+		
 	}
 }
